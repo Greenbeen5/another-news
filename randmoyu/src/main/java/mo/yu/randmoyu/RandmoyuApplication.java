c28fb3555c5d2af1fe1b097810ed9b0784bb31bd
greenbeen5 <greenbeen5@outlook.com>
2020-11-03 22:55:42 +0800

    Creation

---
+++ randmoyu/src/main/java/mo/yu/randmoyu/RandmoyuApplication.java
@@ -0,0 +1,13 @@
+package mo.yu.randmoyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandmoyuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandmoyuApplication.class, args);
+	}
+
+}
---
+++ randmoyu/src/test/java/mo/yu/randmoyu/RandmoyuApplicationTests.java
@@ -0,0 +1,13 @@
+package mo.yu.randmoyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandmoyuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
